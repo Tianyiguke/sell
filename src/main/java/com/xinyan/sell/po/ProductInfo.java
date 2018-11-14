23370cc31fa2f896eb6b01bdbc35d62627f8530d
stupid <1164724823@qq.com>
2018-11-14 16:53:43 +0800

    ProductInfo Test、ProductInfoService

--- src/main/java/com/xinyan/sell/po/ProductInfo.java
+++ src/main/java/com/xinyan/sell/po/ProductInfo.java
@@ -4,8 +4,10 @@ import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * 不夏
@@ -21,6 +23,7 @@ import java.math.BigDecimal;
 public class ProductInfo {
 
     /*商品ID*/
+    @Id
     private String productId;
 
     /*商品名称*/
@@ -39,10 +42,13 @@ public class ProductInfo {
     private String productIcon;
 
     /*商品状态*/
-    private Integer productStatus;
+    private Integer productStatus = 0;
 
     /*类目编号*/
     private Integer categoryType;
+
+    /*创建时间*/
+    private Date createTime;
 
 
 }

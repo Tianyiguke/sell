d07ef26f6a4d356bbab053a43c230b00aa049969
haoyuam0410 <haoyuan0410@163.com>
2018-11-14 16:58:03 +0800

    Utils

--- src/test/java/com/xinyan/sell/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/xinyan/sell/repository/ProductCategoryRepositoryTest.java
@@ -13,9 +13,30 @@ import static org.junit.Assert.*;
 public class ProductCategoryRepositoryTest {
             @Autowired
             private ProductCategoryRepository repository;
-            @Test
-            public void findoneTest(){
+
+    /**
+     * 查询单个对象
+     */
+    @Test
+    public void findoneTest(){
                 ProductCategory productCategory = repository.findOne(1);
-                Assert.assertNotNull(productCategory);
-            }
+                Assert.assertNotNull("根据ID查询类目",productCategory);
+    }
+    /**
+     * 保存对象
+     *
+     */
+    @Test
+    public void saveTest(){
+        ProductCategory productCategory=new ProductCategory();
+        productCategory.setCategoryName("女神-精");
+        productCategory.setCategoryType(7);
+        ProductCategory category = repository.save(productCategory);
+        Assert.assertNotNull("添加商品类目",category);
+    }
+    @Test
+    public void findByCategoryNameLikeTest(){
+        ProductCategory productCategory = repository.findByCategoryNameLike("%女%");
+        System.out.println(productCategory);
+    }
 }

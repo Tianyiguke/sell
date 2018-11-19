c33b8e51e2b81eb2c2e57030cb752b42439d3b6d
GayGaysh <527428049@qq.com>
2018-11-19 14:59:27 +0800

    2018-11-19 创建订单方法实现2

--- src/main/java/com/xinyan/sell/dto/CardDTO.java
+++ src/main/java/com/xinyan/sell/dto/CardDTO.java
@@ -1,6 +1,20 @@
+package com.xinyan.sell.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
 /**
  * Administrator
  * 2018/11/19 11:23
+ * 订单商品
  */
+@Data
 public class CardDTO {
+    /* 商品编号 */
+    @JsonProperty("productId")
+    private String productId;
+
+    /* 商品数量 */
+    @JsonProperty("productQuantity")
+    private Integer productQuantity;
 }
